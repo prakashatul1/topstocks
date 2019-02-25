23cd2b0d37ed00ba150ef3774555027409f3262a
Kumar Atul Prakash <kumar.p@Atul-Prakash.local>
2019-02-25 13:09:13 +0530

    ui changes

--- web.py
+++ web.py
@@ -27,15 +27,24 @@ class index:
 
         template = env.get_template('index.html')
 
+        maxm = 0
+        for i in conn.keys('*'):
+            try:
+                i = int(i.decode('utf-8'))
+                if i > maxm: maxm = i
+            except:
+                continue
+
         list_of_hash = []
-        for i in range(3000):
+        for i in range(maxm+1):
             if conn.hget(i, 'SC_NAME') == None:
                 continue
             if search.lower().strip() in conn.hget(i, 'SC_NAME').decode('utf-8').strip().lower():
                 data_dict = {k.decode('utf-8'): v.decode('utf-8') for (k, v) in conn.hgetall(i).items()}
                 list_of_hash.append(data_dict)
 
-        return template.render(top_10=list_of_hash)
+
+        return template.render(top_10=list_of_hash, search=search, search_flag = True)
 
 
 conn = redis.Redis('localhost')
